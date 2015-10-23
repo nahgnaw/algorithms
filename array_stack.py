fc1b1312f8bded916988af903d872bae31726fd7
Han Wang <wanghan15@gmail.com>
2015-10-23 00:17:34 -0400

    Add queue implementation.

--- array_stack.py
+++ array_stack.py
@@ -34,23 +34,13 @@ if __name__ == '__main__':
 
     s = ArrayStack()
 
-    print 'Empty stack? {0}'.format(s.is_empty())
+    print 'Empty stack? {}'.format(s.is_empty())
 
-    s.push('a')
-    s.push('b')
-    s.push('c')
+    for i in xrange(5):
+        s.push(i)
 
-    print 'Stack length: {0}'.format(len(s))
-    print 'Stack top: {0}'.format(s.top())
+    print 'Stack length: {}'.format(len(s))
+    print 'Stack top: {}'.format(s.top())
 
-    s.pop()
-    print 'Stack length: {0}'.format(len(s))
-
-    s.pop()
-    print 'Stack length: {0}'.format(len(s))
-
-    s.pop()
-    print 'Stack length: {0}'.format(len(s))
-
-    s.pop()
-    print 'Stack length: {0}'.format(len(s))
+    for i in xrange(5):
+        print s.pop()

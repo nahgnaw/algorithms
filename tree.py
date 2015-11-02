39d5b46642dfe7b24f544c9d9232e4f95182a823
Han Wang <wanghan15@gmail.com>
2015-11-01 21:49:15 -0500

    Add preorder and postorder in the tree class.

--- tree.py
+++ tree.py
@@ -1,6 +1,7 @@
 # -*- coding: utf8 -*-
 
 from abc import ABCMeta, abstractmethod
+from linked_queue import LinkedQueue
 
 
 class Tree(object):
@@ -20,6 +21,10 @@ class Tree(object):
         def __ne__(self, other):
             return not (self == other)
 
+    def __iter__(self):
+        for p in self.positions():
+            yield p.element()
+
     @abstractmethod
     def root(self):
         return
@@ -34,7 +39,7 @@ class Tree(object):
 
     @abstractmethod
     def children(self, p):
-        return
+        yield
 
     @abstractmethod
     def __len__(self):
@@ -60,3 +65,38 @@ class Tree(object):
             return 0
         else:
             return 1 + max(self.height(parent) for parent in self.children(p))
+
+    def positions(self):
+        return self.preorder()
+
+    def _subtree_preorder(self, p):
+        yield p
+        for c in self.children(p):
+            for other in self._subtree_preorder(c):
+                yield other
+
+    def preorder(self):
+        if not self.is_empty():
+            for p in self._subtree_preorder(self.root()):
+                yield p
+
+    def _subtree_postorder(self, p):
+        for c in self.children(p):
+            for other in self._subtree_postorder(c):
+                yield other
+        yield p
+
+    def postorder(self):
+        if not self.is_empty():
+            for p in self._subtree_postorder(self.root()):
+                yield p
+
+    def breadth_first(self):
+        if not self.is_empty():
+            fringe = LinkedQueue()
+            fringe.enqueue(self.root())
+            while not fringe.is_empty():
+                p = fringe.dequeue()
+                yield p
+                for c in self.children(p):
+                    fringe.enqueue(c)

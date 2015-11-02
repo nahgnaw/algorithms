9e29a647ce521d46a4d9a531e444d20693febec8
Han Wang <wanghan15@gmail.com>
2015-11-01 22:02:09 -0500

    Add inorder in the binary tree class.

--- binary_tree.py
+++ binary_tree.py
@@ -30,3 +30,20 @@ class BinaryTree(Tree):
             yield self.left(p)
         if self.right(p) is not None:
             yield self.right(p)
+
+    def positions(self):
+        return self.inorder()
+
+    def _subtree_inorder(self, p):
+        if self.left(p) is not None:
+            for other in self._subtree_inorder(self.left(p)):
+                yield other
+        yield p
+        if self.right(p) is not None:
+            for other in self._subtree_inorder(self.right(p)):
+                yield other
+
+    def inorder(self):
+        if not self.is_empty():
+            for p in self._subtree_inorder(self.root()):
+                yield p

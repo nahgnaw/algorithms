f7e5fdf9939f51d8b3ca14509c8163fa44e03c89
Han Wang <wanghan15@gmail.com>
2015-11-07 14:37:27 -0500

    Add heap priority queue implementation.

--- priority_queue.py
+++ priority_queue.py
@@ -87,3 +87,66 @@ class SortedPriorityQueue(PriorityQueueBase):
             raise Empty('Priority queue is empty.')
         item = self._data.delete(self._data.first())
         return item._key, item._value
+
+
+class HeapPriorityQueue(PriorityQueueBase):
+
+    def _parent(self, j):
+        return (j - 1) // 2
+
+    def _left(self, j):
+        return 2 * j + 1
+
+    def _right(self, j):
+        return 2 * j + 2
+
+    def _has_left(self, j):
+        return self._left(j) < len(self._data)
+
+    def _has_right(self, j):
+        return self._right(j) < len(self._data)
+
+    def _swap(self, i, j):
+        self._data[i], self._data[j] = self._data[j], self._data[i]
+
+    def _upheap(self, j):
+        parent = self._parent(j)
+        if j > 0 and self._data[j] < self._data[parent]:
+            self._swap(j, parent)
+            self._upheap(parent)
+
+    def _downheap(self, j):
+        if self._has_left(j):
+            left = self._left(j)
+            smaller_child = left
+            if self._has_right(j):
+                right = self._right(j)
+                if self._data[right] < self._data[left]:
+                    smaller_child = right
+                if self._data[smaller_child] < self._data[j]:
+                    self._swap(smaller_child, j)
+                    self._downheap(smaller_child)
+
+    def __init__(self):
+        self._data = []
+
+    def __len__(self):
+        return len(self._data)
+
+    def add(self, key, value):
+        self._data.append(self._Item(key, value))
+        self._upheap(len(self._data) - 1)
+
+    def min(self):
+        if self.is_empty():
+            raise Empty('Priority queue is empty.')
+        item = self._data[0]
+        return item._key, item._value
+
+    def remove_min(self):
+        if self.is_empty():
+            raise Empty('Priority queue is empty.')
+        self._swap(0, len(self._data) - 1)
+        item = self._data.pop()
+        self._downheap(0)
+        return item._key, item._value

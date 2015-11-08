2d4a6933237591294b5f4e3f547f48911e9f8666
Han Wang <wanghan15@gmail.com>
2015-11-07 21:29:07 -0500

    Add map base class.

---
+++ map_base.py
@@ -0,0 +1,26 @@
+# -*- coding: utf8 -*-
+
+from abc import ABCMeta
+from collections import MutableMapping
+
+
+class MapBase(MutableMapping):
+
+    __metaclass__ = ABCMeta
+
+    class _Item(object):
+
+        __slots__ = '_key', '_value'
+
+        def __init__(self, k, v):
+            self._key = k
+            self._value = v
+
+        def __eq__(self, other):
+            return self._key == other._key
+
+        def __ne__(self, other):
+            return not (self == other)
+
+        def __lt__(self, other):
+            return self._key < other._key

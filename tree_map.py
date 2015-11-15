6fbd658f849784f439dd6fc4fca6ae1da572a5a0
Han Wang <wanghan15@gmail.com>
2015-11-14 20:41:24 -0500

    Add SplayTreeMap.

--- tree_map.py
+++ tree_map.py
@@ -250,3 +250,29 @@ class AVLTreeMap(TreeMap):
 
     def _rebalance_delete(self, p):
         self._rebalance(p)
+
+
+class SplayTreeMap(TreeMap):
+
+    def _splay(self, p):
+        while p != self.root():
+            parent = self.parent(p)
+            grand = self.parent(parent)
+            if grand is None:
+                self._rotate(p)
+            elif (parent == self.left(grand)) == (p == self.left(parent)):
+                self._rotate(parent)
+                self._rotate(p)
+            else:
+                self._rotate(p)
+                self._rotate(p)
+
+    def _rebalance_insert(self, p):
+        self._splay(p)
+
+    def _rebalance_delete(self, p):
+        if p is not None:
+            self._splay(p)
+
+    def _rebalance_access(self, p):
+        self._splay(p)

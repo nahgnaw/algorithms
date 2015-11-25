8c5dc066d12dfb0140d3ef20b2840404e106bb4c
Han Wang <wanghan15@gmail.com>
2015-11-24 23:52:59 -0500

    Add dfs and bfs.

---
+++ breadth_first_search.py
@@ -0,0 +1,14 @@
+# -*- coding: utf8 -*-
+
+
+def breadth_first_search(g, s, discovered):
+    level = [s]
+    while len(level) > 0:
+        next_level = []
+        for u in level:
+            for e in g.incident_edges(u):
+                v = e.opposite(u)
+                if v not in discovered:
+                    discovered[v] = e
+                    next_level.append(v)
+        level = next_level
